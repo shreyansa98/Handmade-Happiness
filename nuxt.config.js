4b92442990b028ac4dcdd46e1c49f4e76f61cb66
Shreyansa Sunder Das <shreyansa1998@gmail.com>
2020-07-13 20:43:06 +0530

    /

--- nuxt.config.js
+++ nuxt.config.js
@@ -129,7 +129,7 @@ export default {
       },
       google: {
         client_id: '303567690628-4bb98e9nc16uno918hecga17hotajp3t.apps.googleusercontent.com',
-        redirect_uri: 'https://shreyansa98.github.io/hh/home/',
+        redirect_uri: 'https://shreyansa98.github.io/hh/home',
       }
     }
   }

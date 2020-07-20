69753fcfb10c86b289ef3960c8e02052ecf94fdb
Shreyansa Sunder Das <shreyansa1998@gmail.com>
2020-07-21 00:19:04 +0530

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || '',
+    title: 'Handmade Happiness',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
